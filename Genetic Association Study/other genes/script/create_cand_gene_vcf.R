b7f2a3dd0e7e4435b36836453fca3f6802a938fe
Kathy Nevola <kathleen.nevola@tufts.edu>
2020-04-13 11:20:31 -0400

    Updated with New Candidate Gene List

--- Genetic Association Study/other genes/script/create_cand_gene_vcf.R
+++ Genetic Association Study/other genes/script/create_cand_gene_vcf.R
@@ -1,20 +1,29 @@
 # extracting gene portion from vcf files: 
 #Read gene list:
 rm(list=ls())
+library(dplyr)
 library(xlsx)
 options(stringsAsFactors = F)
 setwd("/home/clary@mmcf.mehealth.org/Framingham/OmicData/MMC/Genetic Association Study/other genes/data/")
-gene_list1 <- read.xlsx("Candidate Gene List.xlsx", sheetIndex = 1)
+gene_list1 <- read.xlsx("CandidateGeneList_4_10.xlsx", sheetIndex = 1)
 gene_list <- gene_list1 %>% filter(., Selected.for.analysis == "X")
 
 #####extract gene from the chromosome: 
 extract <-function(gene_list) {
   for (i in 1:nrow(gene_list)) {
-    print(i)
-    cmd = "vcftools --gzvcf chr"
-    cmd = paste(cmd,gene_list$Chromosome[i],"_c1.vcf.gz", " --chr ",gene_list$Chromosome[i]," --from-bp ",gene_list$Min[i]," --to-bp ",gene_list$Max[i], sep = "")
-    cmd = paste(cmd," --positions WellImputedPositions8.txt --keep IndivWithPheno.txt --recode ", "--out ",gene_list$Gene.Symbol[i],sep="")
-    system(cmd)
+    if (gene_list$Chromosome == 10){
+      print(i)
+      cmd = "vcftools --vcf chr"
+      cmd = paste(cmd,gene_list$Chromosome[i],"_c1.vcf", " --chr ",gene_list$Chromosome[i]," --from-bp ",gene_list$Min[i]," --to-bp ",gene_list$Max[i], sep = "")
+      cmd = paste(cmd," --positions WellImputedPositions8.txt --keep IndivWithPheno.txt --recode ", "--out ",gene_list$Gene.Symbol[i],sep="")
+      system(cmd)
+    }else{
+      print(i)
+      cmd = "vcftools --gzvcf chr"
+      cmd = paste(cmd,gene_list$Chromosome[i],"_c1.vcf.gz", " --chr ",gene_list$Chromosome[i]," --from-bp ",gene_list$Min[i]," --to-bp ",gene_list$Max[i], sep = "")
+      cmd = paste(cmd," --positions WellImputedPositions8.txt --keep IndivWithPheno.txt --recode ", "--out ",gene_list$Gene.Symbol[i],sep="")
+      system(cmd)
+    }
   }
 }
 
