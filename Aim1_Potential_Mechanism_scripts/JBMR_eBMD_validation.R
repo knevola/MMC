e18cd57493a6c98882c21434ee681559f84afd3a
Kathy Nevola <kathleen.nevola@tufts.edu>
2020-06-04 11:17:33 -0400

    Calculate miRNA z-score

--- Aim1_Potential_Mechanism_scripts/JBMR_eBMD_validation.R
+++ Aim1_Potential_Mechanism_scripts/JBMR_eBMD_validation.R
@@ -26,6 +26,9 @@ info1$`hsa-miR-186-5p` <- as.numeric(info1$`hsa-miR-186-5p`)
 
 
 bmd_info <- merge(bmd_data, info1, by.x = "PatNr", by.y = "ID")
+bmd_info$Z_miR_19 <- (bmd_info$`hsa-miR-19a-3p` - mean(bmd_info$`hsa-miR-19a-3p`))/sd(bmd_info$`hsa-miR-19a-3p`)
+bmd_info$Z_miR_186 <- (bmd_info$`hsa-miR-186-5p` - mean(bmd_info$`hsa-miR-186-5p`))/sd(bmd_info$`hsa-miR-186-5p`)
+
 
 t.test(eBMD..Lunar.heel.ultrasound. ~ Group, bmd_info)
 
@@ -46,10 +49,16 @@ cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-19a-3p`)
 cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-186-5p`)
 
 summary(lm(`hsa-miR-19a-3p`~eBMD..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. , data = bmd_info))
+summary(lm(Z_miR_19~eBMD..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. , data = bmd_info))
+
 summary(lm(`hsa-miR-186-5p`~eBMD..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. , data = bmd_info))
+summary(lm(Z_miR_186~eBMD..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. , data = bmd_info))
 
 summary(lm(`hsa-miR-19a-3p`~Z.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
+summary(lm(Z_miR_19~Z.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
 summary(lm(`hsa-miR-186-5p`~Z.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
+summary(lm(Z_miR_186~Z.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
+
 
 summary(lm(`hsa-miR-19a-3p`~T.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
 summary(lm(`hsa-miR-186-5p`~T.Score..Lunar.heel.ultrasound. + Height.cm + Weight.kg + Age. + Sex, data = bmd_info))
@@ -75,6 +84,8 @@ summary(bmd_info$T.Score..Lunar.heel.ultrasound.)
 cor.test.plus(cor.test(bmd_info$T.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-19a-3p`))
 cor.test.plus(cor.test(bmd_info$T.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-186-5p`))
 cor.test.plus(cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-19a-3p`))
+cor.test.plus(cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$Z_miR_19))
 cor.test.plus(cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$`hsa-miR-186-5p`))
+cor.test.plus(cor.test(bmd_info$Z.Score..Lunar.heel.ultrasound., bmd_info$Z_miR_186))
 
 
