66b7344707c488fd2fdd300221580b66471909e5
Kathy Nevola <kathleen.nevola@tufts.edu>
2020-08-18 13:09:23 -0400

    added comments for future users

--- Aim2_Genetic_Influences_scripts/Malta_data_analysis.R
+++ Aim2_Genetic_Influences_scripts/Malta_data_analysis.R
@@ -20,8 +20,8 @@ hist(data$Age) #Not Normally distributed
 hist(data$Height)
 hist(data$BMI) #Long right tail
 
-rs124_ADRB1<-lm("FN_BMD ~ BB*rs12414657..T.C. + Age + Height + BMI", data = data)
+rs124_ADRB1<-lm("FN_BMD ~ BB*rs12414657..T.C. + Age + Height + BMI", data = data) # No estrogen use available
 summary(rs124_ADRB1)
-rs111_HDAC4 <- lm("FN_BMD ~ BB*rs11124190..C.G. + Age + Height + BMI", data = data)
+rs111_HDAC4 <- lm("FN_BMD ~ BB*rs11124190..C.G. + Age + Height + BMI", data = data) # No estrogen use available
 summary(rs111_HDAC4)
 
