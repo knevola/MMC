a3b32400a585f1e5b2ac62807875227806e7ac9e
Kathy Nevola <kathleen.nevola@tufts.edu>
2020-09-21 13:55:38 -0400

    Update syntax to calculate means

--- Aim2_Genetic_Influences_scripts/study_sample_table.R
+++ Aim2_Genetic_Influences_scripts/study_sample_table.R
@@ -5,5 +5,7 @@ library(gtsummary)
 load("CandidateGenes_4_2020.RData")
 pheno <- pheno_snp_pos$data[[1]]
 pheno1 <- pheno[18:38]
-pheno1 %>% tbl_summary(.,by = "SEX",statistic = list(..continuous.. = "{mean} ({sd})",..categorical.. = "{n} / {N} ({p}%)")) 
-pheno1 %>% tbl_summary(., statistic = list(..continuous.. = "{mean} ({sd})",..categorical.. = "{n} / {N} ({p}%)")) 
+pheno1 %>% tbl_summary(.,by = "SEX",statistic = list(all_continuous() ~ "{mean} ({sd})",
+                                                     all_categorical() ~ "{n} / {N} ({p}%)")) 
+pheno1 %>% tbl_summary(., statistic = list(all_continuous() ~ "{mean} ({sd})",
+                                           all_categorical() ~ "{n} / {N} ({p}%)")) 
